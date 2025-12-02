aa417057b352777184435fd6ea0a006513f7ce19
Gabriel Thompson <gabriel.thomp@cubixdev.org>
2025-12-02 16:34:07 -0500

    fix some weird redefinition bug
    no idea why this error showed up 6 months I wrote this but alright ig

--- input/input.c
+++ input/input.c
@@ -17,6 +17,95 @@ const struct wl_keyboard_listener keys_listener = {
     .repeat_info = NULL
 };
 
+const FiniteKeyMapping finite_key_lookup[] = {
+    // Letters
+    { "A", FINITE_KEY_A, KEY_A }, { "B", FINITE_KEY_B, KEY_B }, { "C", FINITE_KEY_C, KEY_C },
+    { "D", FINITE_KEY_D, KEY_D }, { "E", FINITE_KEY_E, KEY_E }, { "F", FINITE_KEY_F, KEY_F },
+    { "G", FINITE_KEY_G, KEY_G }, { "H", FINITE_KEY_H, KEY_H }, { "I", FINITE_KEY_I, KEY_I },
+    { "J", FINITE_KEY_J, KEY_J }, { "K", FINITE_KEY_K, KEY_K }, { "L", FINITE_KEY_L, KEY_L },
+    { "M", FINITE_KEY_M, KEY_M }, { "N", FINITE_KEY_N, KEY_N }, { "O", FINITE_KEY_O, KEY_O },
+    { "P", FINITE_KEY_P, KEY_P }, { "Q", FINITE_KEY_Q, KEY_Q }, { "R", FINITE_KEY_R, KEY_R },
+    { "S", FINITE_KEY_S, KEY_S }, { "T", FINITE_KEY_T, KEY_T }, { "U", FINITE_KEY_U, KEY_U },
+    { "V", FINITE_KEY_V, KEY_V }, { "W", FINITE_KEY_W, KEY_W }, { "X", FINITE_KEY_X, KEY_X },
+    { "Y", FINITE_KEY_Y, KEY_Y }, { "Z", FINITE_KEY_Z, KEY_Z },
+
+    // Digits
+    { "0", FINITE_KEY_0, KEY_0 }, { "1", FINITE_KEY_1, KEY_1 }, { "2", FINITE_KEY_2,KEY_2 },
+    { "3", FINITE_KEY_3, KEY_3 }, { "4", FINITE_KEY_4, KEY_4 }, { "5", FINITE_KEY_5, KEY_5 },
+    { "6", FINITE_KEY_6, KEY_6 }, { "7", FINITE_KEY_7, KEY_7 }, { "8", FINITE_KEY_8, KEY_8 },
+    { "9", FINITE_KEY_9, KEY_9 },
+
+    // Modifiers
+    { "LeftShift",  FINITE_KEY_LEFT_SHIFT, KEY_LEFTSHIFT },   { "RightShift", FINITE_KEY_RIGHT_SHIFT, KEY_RIGHTSHIFT },
+    { "LeftCtrl",   FINITE_KEY_LEFT_CTRL, KEY_LEFTCTRL },    { "RightCtrl",  FINITE_KEY_RIGHT_CTRL, KEY_RIGHTCTRL },
+    { "LeftAlt",    FINITE_KEY_LEFT_ALT, KEY_LEFTALT },     { "RightAlt",   FINITE_KEY_RIGHT_ALT, KEY_RIGHTALT },
+    { "LeftMeta",   FINITE_KEY_LEFT_META, KEY_LEFTMETA },    { "RightMeta",  FINITE_KEY_RIGHT_META, KEY_RIGHTMETA },
+
+    // Arrows
+    { "Up",    FINITE_KEY_UP, KEY_UP },
+    { "Down",  FINITE_KEY_DOWN, KEY_DOWN },
+    { "Left",  FINITE_KEY_LEFT, KEY_LEFT },
+    { "Right", FINITE_KEY_RIGHT, KEY_RIGHT },
+
+    // Function keys
+    { "F1", FINITE_KEY_F1, KEY_F1 }, { "F2", FINITE_KEY_F2, KEY_F2 }, { "F3", FINITE_KEY_F3, KEY_F3 },
+    { "F4", FINITE_KEY_F4, KEY_F4 }, { "F5", FINITE_KEY_F5, KEY_F5 }, { "F6", FINITE_KEY_F6, KEY_F6 },
+    { "F7", FINITE_KEY_F7, KEY_F7 }, { "F8", FINITE_KEY_F8, KEY_F8 }, { "F9", FINITE_KEY_F9, KEY_F9 },
+    { "F10", FINITE_KEY_F10, KEY_F10 }, { "F11", FINITE_KEY_F11, KEY_F11 }, { "F12", FINITE_KEY_F12, KEY_F12 },
+
+    // Editing / navigation
+    { "Enter",      FINITE_KEY_ENTER, KEY_ENTER },
+    { "Escape",     FINITE_KEY_ESCAPE, KEY_ESC },
+    { "Backspace",  FINITE_KEY_BACKSPACE, KEY_BACKSPACE },
+    { "Tab",        FINITE_KEY_TAB, KEY_TAB },
+    { "Space",      FINITE_KEY_SPACE, KEY_SPACE },
+    { "CapsLock",   FINITE_KEY_CAPS_LOCK, KEY_CAPSLOCK },
+    { "Insert",     FINITE_KEY_INSERT, KEY_INSERT },
+    { "Delete",     FINITE_KEY_DELETE, KEY_DELETE },
+    { "Home",       FINITE_KEY_HOME, KEY_HOME },
+    { "End",        FINITE_KEY_END, KEY_END },
+    { "PageUp",     FINITE_KEY_PAGE_UP, KEY_PAGEUP },
+    { "PageDown",   FINITE_KEY_PAGE_DOWN, KEY_PAGEDOWN },
+
+    // Meta
+    { "PrintScreen", FINITE_KEY_PRINT_SCREEN, KEY_PRINT },
+    { "ScrollLock",  FINITE_KEY_SCROLL_LOCK, KEY_SCROLLLOCK },
+    { "Pause",       FINITE_KEY_PAUSE, KEY_PAUSE },
+
+    // Symbols and punctuation
+    { "Minus",        FINITE_KEY_MINUS, KEY_MINUS },
+    { "Equals",       FINITE_KEY_EQUALS, KEY_EQUAL },
+    { "LeftBracket",  FINITE_KEY_LEFT_BRACKET, KEY_LEFTBRACE },
+    { "RightBracket", FINITE_KEY_RIGHT_BRACKET, KEY_RIGHTBRACE },
+    { "Backslash",    FINITE_KEY_BACKSLASH, KEY_BACKSLASH },
+    { "Semicolon",    FINITE_KEY_SEMICOLON, KEY_SEMICOLON },
+    { "Apostrophe",   FINITE_KEY_APOSTROPHE, KEY_APOSTROPHE },
+    { "Grave",        FINITE_KEY_GRAVE, KEY_GRAVE },
+    { "Comma",        FINITE_KEY_COMMA, KEY_COMMA },
+    { "Period",       FINITE_KEY_PERIOD, KEY_DOT },
+    { "Slash",        FINITE_KEY_SLASH, KEY_SLASH },
+
+    // Numpad
+    { "NumLock",      FINITE_KEY_NUM_LOCK, KEY_NUMLOCK },
+    { "KpDivide",     FINITE_KEY_KP_DIVIDE, KEY_KPSLASH },
+    { "KpMultiply",   FINITE_KEY_KP_MULTIPLY, KEY_KPASTERISK },
+    { "KpMinus",      FINITE_KEY_KP_MINUS, KEY_KPMINUS },
+    { "KpPlus",       FINITE_KEY_KP_PLUS, KEY_KPPLUS },
+    { "KpEnter",      FINITE_KEY_KP_ENTER, KEY_KPENTER },
+    { "Kp0",          FINITE_KEY_KP_0, KEY_KP0 },
+    { "Kp1",          FINITE_KEY_KP_1, KEY_KP1 },
+    { "Kp2",          FINITE_KEY_KP_2, KEY_KP2 },
+    { "Kp3",          FINITE_KEY_KP_3, KEY_KP3 },
+    { "Kp4",          FINITE_KEY_KP_4, KEY_KP4 },
+    { "Kp5",          FINITE_KEY_KP_5, KEY_KP5 },
+    { "Kp6",          FINITE_KEY_KP_6, KEY_KP6 },
+    { "Kp7",          FINITE_KEY_KP_7, KEY_KP7 },
+    { "Kp8",          FINITE_KEY_KP_8, KEY_KP8 },
+    { "Kp9",          FINITE_KEY_KP_9, KEY_KP9 },
+    { "KpPeriod",     FINITE_KEY_KP_PERIOD, KEY_KPDOT },
+};
+
+
 FiniteKeyboard *finite_input_keyboard_init_debug(const char *file, const char *func, int line, struct wl_display *device) {
     FiniteKeyboard *keyboard = calloc(1, sizeof(FiniteKeyboard));
     keyboard->input = calloc(1, sizeof(FiniteInput));
