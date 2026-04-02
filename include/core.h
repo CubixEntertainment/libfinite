846d135bc322d619d96081aced0968ee2d8bd90a
Gabriel Thompson <gabriel.thomp@cubixdev.org>
2026-04-02 00:05:57 -0400

    small fix

--- include/core.h
+++ include/core.h
@@ -1,5 +1,6 @@
 #ifndef __CORE_H__
 #define __CORE_H__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -11,4 +12,5 @@ typedef struct {
 
 char* finite_readfile(const char* filename, size_t* fileSize);
 FileBuffer finite_read_raw_file(const char* filename);
+
 #endif
